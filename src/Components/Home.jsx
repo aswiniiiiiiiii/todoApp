ee4748cbe4771ba94dc2892d7a0b1aa879ee159d
aswiniiiiiiiii <aswinips787@gmail.com>
2024-10-29 21:39:24 +0530

    todoApp-Frontend

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -95,59 +95,61 @@ const Home = () => {
 
     return (
         <>
-            <div style={{ height: "100vh" }} className="d-flex justify-content-center">
-                <div className='second-section'>
-                    <div className='second-sub-section d-flex flex-column mt-5 justify-content-center align-items-center ps-3'>
-                        <h1 className='fw-bolder fs-1'>TODO APP</h1>
-                        <h4>{time.toLocaleTimeString('en-us', {
-                            day: 'numeric',
-                            weekday: 'long',
-                            year: 'numeric'
-                        })}</h4>
-                        <div className='search-box d-flex justify-content-center align-items-center'>
-                            <div style={{ width: '50%' }} className='search'>
-                                <input
-                                    value={input.tasks}
-                                    onChange={(e) => setInputs({ ...input, tasks: e.target.value })}
-                                    className='form-control py-2'
-                                    placeholder='Enter your task..'
-                                    type="text"
-                                />
+            <div className='main-section'>
+                <div style={{ height: "100vh" }} className=" d-flex justify-content-center">
+                    <div className='second-section'>
+                        <div className='second-sub-section d-flex flex-column mt-5 justify-content-center align-items-center ps-3'>
+                            <h1 className='fw-bolder fs-1'>TODO APP</h1>
+                            <h4>{time.toLocaleTimeString('en-us', {
+                                day: 'numeric',
+                                weekday: 'long',
+                                year: 'numeric'
+                            })}</h4>
+                            <div className='search-box d-flex justify-content-center align-items-center'>
+                                <div style={{ width: '50%' }} className='search'>
+                                    <input
+                                        value={input.tasks}
+                                        onChange={(e) => setInputs({ ...input, tasks: e.target.value })}
+                                        className='form-control py-2'
+                                        placeholder='Enter your task..'
+                                        type="text"
+                                    />
+                                </div>
+                                <button onClick={handleNotes} className='btn btn-primary ms-3 shadow'>
+                                    {editingId ? "Update Task" : "Add Task"}
+                                </button>
                             </div>
-                            <button onClick={handleNotes} className='btn btn-primary ms-3 shadow'>
-                                {editingId ? "Update Task" : "Add Task"}
-                            </button>
                         </div>
-                    </div>
-                    <div className='d-flex justify-content-center align-items-center'>
-                        <div className='third-section'>
-                            <div className='firstbtn d-flex justify-content-between align-items-center m-3'>
-                                <Link to={'History'} style={{ textDecoration: 'none' }} className='text-light'>
-                                    <h5 className='p-2'><i className="fa-solid fa-list-check pe-3"></i>Completed Tasks</h5>
-                                </Link>
-                                <p className='fs-5 text-light mt-1 pe-2 me-3'>{completedTasksCount}</p> {/* Displaying Completed Task Count */}
+                        <div className='main-section  d-flex justify-content-center align-items-center'>
+                            <div className='third-section'>
+                                <div className='firstbtn d-flex justify-content-between align-items-center m-3'>
+                                    <Link to={'History'} style={{ textDecoration: 'none' }} className='text-light'>
+                                        <h5 className='p-2'><i className="fa-solid fa-list-check pe-3"></i>Completed Tasks</h5>
+                                    </Link>
+                                    <p className='fs-5 text-light mt-1 pe-2 me-3'>{completedTasksCount}</p> {/* Displaying Completed Task Count */}
+                                </div>
+                                <ul className='my-3 mb-5 me-4'>
+                                    {allNotes?.length > 0 ?
+                                        allNotes?.map(notes => (
+                                            <li className='d-flex justify-content-between shadow border fs-4 mt-3 p-1'>
+                                                <span onClick={() => markAsCompleted(notes)} className='pe-3' style={{ cursor: "pointer" }}>
+                                                    <img width={'25px'} src={square} alt="Complete" /> {notes?.tasks}
+                                                </span>
+                                                <div className='me-4 d-flex justify-content-center align-items-center'>
+                                                    <span onClick={() => startEditNote(notes.id, notes.tasks)} style={{ cursor: "pointer" }} className='pe-3'>
+                                                        <i className="fa-solid fa-edit text-success fs-5"></i>
+                                                    </span>
+                                                    <span onClick={() => deleteNotes(notes?.id)} style={{ cursor: "pointer" }}>
+                                                        <i className="fa-solid fa-trash text-danger fs-5"></i>
+                                                    </span>
+                                                </div>
+                                            </li>
+                                        ))
+                                        :
+                                        <div className="schedule text-center">Schedule your Day!!</div>
+                                    }
+                                </ul>
                             </div>
-                            <ul className='my-3 mb-5 me-4'>
-                                {allNotes?.length > 0 ?
-                                    allNotes?.map(notes => (
-                                        <li className='d-flex justify-content-between shadow border fs-4 mt-3 p-1'>
-                                            <span onClick={() => markAsCompleted(notes)} className='pe-3' style={{ cursor: "pointer" }}>
-                                                <img width={'25px'} src={square} alt="Complete" /> {notes?.tasks}
-                                            </span>
-                                            <div className='me-4 d-flex justify-content-center align-items-center'>
-                                                <span onClick={() => startEditNote(notes.id, notes.tasks)} style={{ cursor: "pointer" }} className='pe-3'>
-                                                    <i className="fa-solid fa-edit text-success fs-5"></i>
-                                                </span>
-                                                <span onClick={() => deleteNotes(notes?.id)} style={{ cursor: "pointer" }}>
-                                                    <i className="fa-solid fa-trash text-danger fs-5"></i>
-                                                </span>
-                                            </div>
-                                        </li>
-                                    ))
-                                    :
-                                    <div className="schedule text-center">Schedule your Day!!</div>
-                                }
-                            </ul>
                         </div>
                     </div>
                 </div>
